e3689363d74652a23fb7ee2c01ead643acb57552
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-11-17 16:49:03 +0900

    Set default sort order for perks

--- app/models/perk.rb
+++ app/models/perk.rb
@@ -1,5 +1,6 @@
 class Perk < ActiveRecord::Base
   belongs_to :skill
+  default_scope order: "skill_points ASC"
   has_and_belongs_to_many :prerequisites, class_name: "Perk", join_table: "perks_prerequisites", foreign_key: "perk_id", association_foreign_key: "prerequisite_id"
 
   def short_name
